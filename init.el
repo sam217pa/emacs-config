0ed3c29da1f6e59f92f57b434eedeaf86e3fa5de
sam217pa <samuel.barreto8@gmail.com>
2016-09-01 14:28:48 +0200

    moved r related config back into init.el

--- init.el
+++ init.el
@@ -250,6 +250,74 @@
   )
 
 ;;; -E-
+(use-package ess-site
+  :ensure ess
+  :mode
+  (("\\.sp\\'"           . S-mode)
+   ("/R/.*\\.q\\'"       . R-mode)
+   ("\\.[qsS]\\'"        . S-mode)
+   ("\\.ssc\\'"          . S-mode)
+   ("\\.SSC\\'"          . S-mode)
+   ("\\.[rR]\\'"         . R-mode)
+   ("\\.[rR]nw\\'"       . Rnw-mode)
+   ("\\.[sS]nw\\'"       . Snw-mode)
+   ("\\.[rR]profile\\'"  . R-mode)
+   ("NAMESPACE\\'"       . R-mode)
+   ("CITATION\\'"        . R-mode)
+   ("\\.omg\\'"          . omegahat-mode)
+   ("\\.hat\\'"          . omegahat-mode)
+   ("\\.lsp\\'"          . XLS-mode)
+   ("\\.do\\'"           . STA-mode)
+   ("\\.ado\\'"          . STA-mode)
+   ("\\.[Ss][Aa][Ss]\\'" . SAS-mode)
+   ("\\.jl\\'"           . ess-julia-mode)
+   ("\\.[Ss]t\\'"        . S-transcript-mode)
+   ("\\.Sout"            . S-transcript-mode)
+   ("\\.[Rr]out"         . R-transcript-mode)
+   ("\\.Rd\\'"           . Rd-mode)
+   ("\\.[Bb][Uu][Gg]\\'" . ess-bugs-mode)
+   ("\\.[Bb][Oo][Gg]\\'" . ess-bugs-mode)
+   ("\\.[Bb][Mm][Dd]\\'" . ess-bugs-mode)
+   ("\\.[Jj][Aa][Gg]\\'" . ess-jags-mode)
+   ("\\.[Jj][Oo][Gg]\\'" . ess-jags-mode)
+   ("\\.[Jj][Mm][Dd]\\'" . ess-jags-mode))
+
+  :commands
+  (R stata julia SAS)
+
+  :init
+  (add-hook 'ess-mode-hook 'company-mode)
+  (add-hook 'ess-mode-hook
+            (lambda () (flycheck-mode)
+              (run-hooks 'prog-mode-hook 'company-mode-hook)))
+  (add-hook 'ess-R-post-run-hook (lambda () (smartparens-mode 1)))
+
+  :config
+  (setq ess-R-font-lock-keywords '((ess-R-fl-keyword:modifiers . t)
+                                   (ess-R-fl-keyword:fun-defs . t)
+                                   (ess-R-fl-keyword:keywords . t)
+                                   (ess-R-fl-keyword:assign-ops . t)
+                                   (ess-R-fl-keyword:constants . t)
+                                   (ess-fl-keyword:fun-calls . t)
+                                   (ess-fl-keyword:numbers . t)
+                                   (ess-fl-keyword:operators . t)
+                                   (ess-fl-keyword:delimiters . t)
+                                   (ess-fl-keyword:= . t)
+                                   (ess-R-fl-keyword:F&T . t)
+                                   (ess-R-fl-keyword:%op% . t)))
+  (setq ess-offset-continued 2
+        ess-expression-offset 2
+        ess-nuke-trailing-whitespace-p t
+        ess-default-style 'DEFAULT)
+
+  (add-to-list
+   'aggressive-indent-dont-indent-if
+   '(and (derived-mode-p 'ess-mode)
+         (null (string-match "\\(#+ .+ $\\)"
+                             (thing-at-point 'line)))))
+
+  )
+
 (use-package evil :ensure t
   :init
   (evil-mode 1)
@@ -784,8 +852,6 @@
 (load-file "~/dotfile/emacs/functions.el")
 ;; keybindings
 (load-file "~/dotfile/emacs/keybindings.el")
-;; R config
-(load-file "~/dotfile/emacs/r.el")
 ;; org
 (load-file "~/dotfile/emacs/org.el")
 
