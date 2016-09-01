7df9813040ed01e05ee5e84408b5d5fed07e47be
sam217pa <samuel.barreto8@gmail.com>
2016-09-01 13:15:46 +0200

    python related config back into init.el

--- init.el
+++ init.el
@@ -161,6 +161,16 @@
   )
 
 ;;; -C-
+
+(use-package color-identifiers-mode :ensure t
+  ;; colore les variables dans certains mode de programmation
+  ;; par une couleur unique
+  :commands color-identifiers-mode
+  :diminish (color-identifiers-mode . "")
+  :init
+  (add-hook 'python-mode-hook 'color-identifiers-mode)
+  )
+
 (use-package color-theme-solarized :ensure t
   :init
   ;; to make the byte compiler happy. emacs25 has no color-themes variable
@@ -506,6 +516,43 @@
   :config (progn
             (setq projectile-completion-system 'ivy)
             (add-to-list 'projectile-globally-ignored-files ".DS_Store")))
+
+
+(use-package python
+  :ensure python-mode
+  :mode
+  (("\\.py\\'" . python-mode)
+   ("\\.wsgi$" . python-mode))
+  :interpreter
+  (("ipython" . python-mode)
+   ("python"  . python-mode))
+  :config
+
+  (use-package elpy :ensure t
+    :commands elpy-mode
+    :init
+    (add-hook 'python-mode-hook 'elpy-mode)
+    :config
+    (electric-indent-local-mode -1)
+    )
+
+  ;; (use-package anaconda-mode :ensure t :defer t
+  ;;   :init
+  ;;   (add-hook 'python-mode-hook 'anaconda-mode)
+  ;;   (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+  ;;   :config
+  ;;   (use-package pyenv-mode :ensure t
+  ;;     :init (pyenv-mode))
+  ;;   )
+
+  (use-package pyenv-mode :ensure t
+    :commands pyenv-mode
+    :init (add-hook 'python-mode-hook 'pyenv-mode)
+    )
+
+  (setq-default indent-tabs-mode nil)
+  (setq python-indent-offset 4)
+  )
 
 ;;; -Q-
 
