647f2d77648030fa715bba53ed61e1062cec4692
sam217pa <samuel.barreto8@gmail.com>
2016-09-27 10:30:45 +0200

    with hydra from wiki

--- init.el
+++ init.el
@@ -1203,7 +1203,29 @@ undo               _u_: undo
   :config
   (general-define-key
    "C-M-s" 'mc/mark-previous-like-this
-   "C-M-t" 'mc/mark-next-like-this)
+   "C-M-t" 'mc/mark-next-like-this
+   "H-SPC" 'hydra-mc/body)
+
+  ;; from https://github.com/abo-abo/hydra/wiki/multiple-cursors
+  (defhydra hydra-mc (:hint nil)
+    "
+     ^Up^            ^Down^        ^Other^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_r_] Mark by regexp
+^ ^             ^ ^             [_q_] Quit
+"
+    ("l" mc/edit-lines :exit t)
+    ("a" mc/mark-all-like-this :exit t)
+    ("n" mc/mark-next-like-this)
+    ("N" mc/skip-to-next-like-this)
+    ("M-n" mc/unmark-next-like-this)
+    ("p" mc/mark-previous-like-this)
+    ("P" mc/skip-to-previous-like-this)
+    ("M-p" mc/unmark-previous-like-this)
+    ("r" mc/mark-all-in-region-regexp :exit t)
+    ("q" nil))
 
   )
 
