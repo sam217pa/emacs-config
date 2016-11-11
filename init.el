29a1cc090a2fa6aade851dc28b76b45ec23988c4
sam217pa <samuel.barreto8@gmail.com>
2016-11-12 00:24:30 +0100

    smartparens hydra support

--- init.el
+++ init.el
@@ -1598,6 +1598,50 @@ _s_: → to    _i_: import   _S_: → to    _C_: kill     _l_: load
   (sp-local-pair 'org-mode "$" "$")
   (sp-pair "'" nil :actions :rem)
 
+  ;; from hydra wiki
+  (defhydra hydra-sp (:hint nil)
+    "
+       ^Navigate^   ^^              ^Slurp - Barf^    ^Splice^          ^Commands^
+       ^--------^   ^^              ^------------^    ^------^          ^--------^
+    _b_: bwd      _f_: fwd       _c_: slurp bwd      _ll_: splice    _x_: del char
+    _T_: bwd ↓    _S_: bwd ↑     _r_: slurp fwd      _lf_: fwd       _é_: del word
+    _t_: ↓        _s_: ↑       _C-c_: barf bwd       _lb_: bwd       _(_: del sexp
+    _p_: prev     _n_: next    _C-r_: barf fwd       _la_: around    _w_: unwrap
+  _M-p_: end    _M-n_: begin   ^   ^                 ^  ^
+       ^------^     ^^             ^------------^
+       ^Symbol^     ^^             ^join - split^
+       ^------^     ^^             ^------------^
+  _C-b_: bwd    _C-b_: fwd       _j_: join
+      ^^            ^^           _v_: split
+"
+    ("b" sp-backward-sexp )
+    ("f" sp-forward-sexp )
+    ("T" sp-backward-down-sexp )
+    ("S" sp-backward-up-sexp )
+    ("t" sp-down-sexp )                 ; root - towards the root
+    ("s" sp-up-sexp )
+    ("n" sp-next-sexp )
+    ("p" sp-previous-sexp )
+    ("M-n" sp-beginning-of-sexp)
+    ("M-p" sp-end-of-sexp )
+    ("x" sp-delete-char )
+    ("é" sp-kill-word )
+    ("(" sp-kill-sexp )
+    ("w" sp-unwrap-sexp ) ;; Strip!
+    ("r" sp-forward-slurp-sexp )
+    ("C-r" sp-forward-barf-sexp )
+    ("c" sp-backward-slurp-sexp )
+    ("C-c" sp-backward-barf-sexp )
+    ("ll" sp-splice-sexp )
+    ("lf" sp-splice-sexp-killing-forward )
+    ("lb" sp-splice-sexp-killing-backward )
+    ("la" sp-splice-sexp-killing-around )
+    ("C-f" sp-forward-symbol )
+    ("C-b" sp-backward-symbol )
+    ("j" sp-join-sexp ) ;;Good
+    ("v" sp-split-sexp )
+    ("u" undo "undo")
+    ("q" nil "quit" :color blue))
 
   (defun sam--create-newline-and-enter-sexp (&rest _ignored)
     "Open a new brace or bracket expression, with relevant newlines and indent. "
